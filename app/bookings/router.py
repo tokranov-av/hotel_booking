c6f23218efbf186ed1bad7668656b10c9333615a
Artur Tokranov <artur.tokranov@gmail.com>
2023-04-22 10:38:39 +0300

    Работаю над проектом

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends
 
 from app.bookings.dao import BookingDAO
 from app.bookings.schemas import SBooking
+from app.exceptions import RoomCannotBeBookedException
 from app.users.dependencies import get_current_user
 from app.users.models import Users
 
@@ -25,4 +26,6 @@ async def add_booking(
         room_id: int, date_from: date, date_to: date,
         user: Users = Depends(get_current_user),
 ):
-    await BookingDAO.add(user.id, room_id, date_from, date_to)
+    booking = await BookingDAO.add(user.id, room_id, date_from, date_to)
+    if not booking:
+        raise RoomCannotBeBookedException
