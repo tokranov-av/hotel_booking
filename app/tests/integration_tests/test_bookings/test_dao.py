cfde7e19e531b5cd5ff780be23008524cc7bd921
Artur Tokranov <artur.tokranov@gmail.com>
2023-05-25 16:42:34 +0300

    Работаю над проектом

--- app/tests/integration_tests/test_bookings/test_dao.py
+++ app/tests/integration_tests/test_bookings/test_dao.py
@@ -16,8 +16,17 @@ async def test_booking_crud(user_id, room_id):
         date_from=datetime.strptime('2023-07-10', '%Y-%m-%d'),
         date_to=datetime.strptime('2023-07-24', '%Y-%m-%d')
     )
-    print(new_booking)
-    assert new_booking.get('user_id') == user_id
-    assert new_booking.get('room_id') == room_id
+    assert new_booking.user_id == user_id
+    assert new_booking.room_id == room_id
 
-    # new_booking = await BookingDAO.find_one_or_none(id=new_booking.id)
+    # Проверка добавления брони
+    new_booking = await BookingDAO.find_one_or_none(id=new_booking.get('id'))
+    assert new_booking is not None
+
+    # Удаление брони
+    await BookingDAO.delete(id=new_booking.get('id'), user_id=user_id)
+
+    # Проверка удаления брони
+    deleted_booking = await BookingDAO.find_one_or_none(
+        id=new_booking.get('id'))
+    assert deleted_booking is None
